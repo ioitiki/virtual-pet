443e6d8f141fd20894bc61a9c91c77c78839e053
Andrew Malkin and oliver fu <oliver@>
2017-04-03 10:23:54 -0700

    complete our tests and methods for Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,70 @@
 import org.sql2o.*;
+import java.util.List;
+import java.util.List;
 
 public class Person {
+  private String name;
+  private int id;
 
-  public Person() {
-
+  public Person(String name) {
+    this.name = name;
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void save() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO persons (name) VALUES (:name);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Person> all(){
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM persons;";
+      return con.createQuery(sql)
+        .executeAndFetch(Person.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherPerson) {
+    if (!(otherPerson instanceof Person)) {
+      return false;
+    } else {
+      Person newPerson = (Person) otherPerson;
+      return this.getName().equals(newPerson.getName()) &&
+             this.getId() == newPerson.getId();
+    }
+  }
+
+  public static Person find(int id) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM persons WHERE id = :id;";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Person.class);
+    }
+  }
+
+  public void updatePerson(String name) {
+    this.name = name;
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE persons SET name = :name WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+ }
 
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -4,17 +4,68 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", null, null);
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void person_instantiatesCorrectly_true() {
+    Person testPerson = new Person("Joe Smith");
+    assertTrue(testPerson instanceof Person);
   }
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
+  @Test
+  public void getName_returnsCorrectName_JoeSmith() {
+    Person testPerson = new Person("Joe Smith");
+    assertEquals("Joe Smith", testPerson.getName());
   }
 
+  @Test
+  public void save_savesPersonToDB_true() {
+    Person testPerson = new Person("Joe Smith");
+    testPerson.save();
+    assertEquals("Joe Smith", Person.all().get(0).getName());
+    assertEquals(testPerson.getId(), Person.all().get(0).getId());
+  }
+
+  @Test
+  public void all_returnsAllPersonsInDB_true() {
+    Person testPerson1 = new Person("Fargo");
+    testPerson1.save();
+    Person testPerson2 = new Person("True Grit");
+    testPerson2.save();
+    assertTrue(Person.all().get(0).equals(testPerson1));
+    assertTrue(Person.all().get(1).equals(testPerson2));
+  }
+
+  @Test
+  public void equals_comparesPersonNameToVerifyEqual_true() {
+    Person testPerson1 = new Person("John Doe");
+    Person testPerson2 = new Person("John Doe");
+    assertTrue(testPerson1.equals(testPerson2));
+  }
+
+  @Test
+  public void getId_assignsIdToPerson_true() {
+    Person testPerson = new Person("John Doe");
+    testPerson.save();
+    assertTrue(testPerson.getId() == Person.all().get(0).getId());
+  }
+
+  @Test
+  public void find_returnsPersonGivenId_Person() {
+    Person testPerson = new Person("John Doe");
+    testPerson.save();
+    int testPersonId = testPerson.getId();
+    assertTrue(Person.find(testPersonId).equals(testPerson));
+  }
+
+  @Test
+  public void update_updatesPersonWithGivenName_true() {
+    Person testPerson = new Person("John Doe");
+    testPerson.save();
+    testPerson.updatePerson("Greg Blah");
+    assertEquals(testPerson.getName(), "Greg Blah");
+    assertEquals(Person.find(testPerson.getId()).getName(), "Greg Blah");
+
+  }
 }
